0f0d922b9cf7b7cbc22d1d39d3a5d749288f26c4
Paidamoyo <ashgirlchapfuwa@gmail.com>
2016-04-15 17:24:18 +0300

    add individual post to view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,10 @@
 from blog.models import Post
 from django.conf.urls import url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 urlpatterns = [
     url(r'^$', ListView.as_view(
         queryset=Post.objects.all().order_by("-date")[:25],
         template_name="blog/blog.html")),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name="blog/post.html"))
 ]
