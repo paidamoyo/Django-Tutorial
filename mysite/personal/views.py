3803865fa03643beab381a8a6b688c94cf064648
Paidamoyo <ashgirlchapfuwa@gmail.com>
2016-04-15 15:25:57 +0300

    pass variables to contact page

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html',
+                  {'content': ['if you would like to contact me, please email me', 'ghkhghjkjk@gmail.com']})
